2e595ef575c4ac37ffd9691a5c78857b4c116461
Sansi <73607360+senseilein@users.noreply.github.com>
2023-01-06 22:40:09 +0000

    Clean code (remove console.logs, format)

--- index.js
+++ index.js
@@ -1,91 +1,91 @@
 // ********** Dataset composed of arrays with two fields, Date and Profit/Losses *********
 var finances = [
-['Jan-2010', 867884],
-['Feb-2010', 984655],
-['Mar-2010', 322013],
-['Apr-2010', -69417],
-['May-2010', 310503],
-['Jun-2010', 522857],
-['Jul-2010', 1033096],
-['Aug-2010', 604885],
-['Sep-2010', -216386],
-['Oct-2010', 477532],
-['Nov-2010', 893810],
-['Dec-2010', -80353],
-['Jan-2011', 779806],
-['Feb-2011', -335203],
-['Mar-2011', 697845],
-['Apr-2011', 793163],
-['May-2011', 485070],
-['Jun-2011', 584122],
-['Jul-2011', 62729],
-['Aug-2011', 668179],
-['Sep-2011', 899906],
-['Oct-2011', 834719],
-['Nov-2011', 132003],
-['Dec-2011', 309978],
-['Jan-2012', -755566],
-['Feb-2012', 1170593],
-['Mar-2012', 252788],
-['Apr-2012', 1151518],
-['May-2012', 817256],
-['Jun-2012', 570757],
-['Jul-2012', 506702],
-['Aug-2012', -1022534],
-['Sep-2012', 475062],
-['Oct-2012', 779976],
-['Nov-2012', 144175],
-['Dec-2012', 542494],
-['Jan-2013', 359333],
-['Feb-2013', 321469],
-['Mar-2013', 67780],
-['Apr-2013', 471435],
-['May-2013', 565603],
-['Jun-2013', 872480],
-['Jul-2013', 789480],
-['Aug-2013', 999942],
-['Sep-2013', -1196225],
-['Oct-2013', 268997],
-['Nov-2013', -687986],
-['Dec-2013', 1150461],
-['Jan-2014', 682458],
-['Feb-2014', 617856],
-['Mar-2014', 824098],
-['Apr-2014', 581943],
-['May-2014', 132864],
-['Jun-2014', 448062],
-['Jul-2014', 689161],
-['Aug-2014', 800701],
-['Sep-2014', 1166643],
-['Oct-2014', 947333],
-['Nov-2014', 578668],
-['Dec-2014', 988505],
-['Jan-2015', 1139715],
-['Feb-2015', 1029471],
-['Mar-2015', 687533],
-['Apr-2015', -524626],
-['May-2015', 158620],
-['Jun-2015', 87795],
-['Jul-2015', 423389],
-['Aug-2015', 840723],
-['Sep-2015', 568529],
-['Oct-2015', 332067],
-['Nov-2015', 989499],
-['Dec-2015', 778237],
-['Jan-2016', 650000],
-['Feb-2016', -1100387],
-['Mar-2016', -174946],
-['Apr-2016', 757143],
-['May-2016', 445709],
-['Jun-2016', 712961],
-['Jul-2016', -1163797],
-['Aug-2016', 569899],
-['Sep-2016', 768450],
-['Oct-2016', 102685],
-['Nov-2016', 795914],
-['Dec-2016', 60988],
-['Jan-2017', 138230],
-['Feb-2017', 671099]
+    ['Jan-2010', 867884],
+    ['Feb-2010', 984655],
+    ['Mar-2010', 322013],
+    ['Apr-2010', -69417],
+    ['May-2010', 310503],
+    ['Jun-2010', 522857],
+    ['Jul-2010', 1033096],
+    ['Aug-2010', 604885],
+    ['Sep-2010', -216386],
+    ['Oct-2010', 477532],
+    ['Nov-2010', 893810],
+    ['Dec-2010', -80353],
+    ['Jan-2011', 779806],
+    ['Feb-2011', -335203],
+    ['Mar-2011', 697845],
+    ['Apr-2011', 793163],
+    ['May-2011', 485070],
+    ['Jun-2011', 584122],
+    ['Jul-2011', 62729],
+    ['Aug-2011', 668179],
+    ['Sep-2011', 899906],
+    ['Oct-2011', 834719],
+    ['Nov-2011', 132003],
+    ['Dec-2011', 309978],
+    ['Jan-2012', -755566],
+    ['Feb-2012', 1170593],
+    ['Mar-2012', 252788],
+    ['Apr-2012', 1151518],
+    ['May-2012', 817256],
+    ['Jun-2012', 570757],
+    ['Jul-2012', 506702],
+    ['Aug-2012', -1022534],
+    ['Sep-2012', 475062],
+    ['Oct-2012', 779976],
+    ['Nov-2012', 144175],
+    ['Dec-2012', 542494],
+    ['Jan-2013', 359333],
+    ['Feb-2013', 321469],
+    ['Mar-2013', 67780],
+    ['Apr-2013', 471435],
+    ['May-2013', 565603],
+    ['Jun-2013', 872480],
+    ['Jul-2013', 789480],
+    ['Aug-2013', 999942],
+    ['Sep-2013', -1196225],
+    ['Oct-2013', 268997],
+    ['Nov-2013', -687986],
+    ['Dec-2013', 1150461],
+    ['Jan-2014', 682458],
+    ['Feb-2014', 617856],
+    ['Mar-2014', 824098],
+    ['Apr-2014', 581943],
+    ['May-2014', 132864],
+    ['Jun-2014', 448062],
+    ['Jul-2014', 689161],
+    ['Aug-2014', 800701],
+    ['Sep-2014', 1166643],
+    ['Oct-2014', 947333],
+    ['Nov-2014', 578668],
+    ['Dec-2014', 988505],
+    ['Jan-2015', 1139715],
+    ['Feb-2015', 1029471],
+    ['Mar-2015', 687533],
+    ['Apr-2015', -524626],
+    ['May-2015', 158620],
+    ['Jun-2015', 87795],
+    ['Jul-2015', 423389],
+    ['Aug-2015', 840723],
+    ['Sep-2015', 568529],
+    ['Oct-2015', 332067],
+    ['Nov-2015', 989499],
+    ['Dec-2015', 778237],
+    ['Jan-2016', 650000],
+    ['Feb-2016', -1100387],
+    ['Mar-2016', -174946],
+    ['Apr-2016', 757143],
+    ['May-2016', 445709],
+    ['Jun-2016', 712961],
+    ['Jul-2016', -1163797],
+    ['Aug-2016', 569899],
+    ['Sep-2016', 768450],
+    ['Oct-2016', 102685],
+    ['Nov-2016', 795914],
+    ['Dec-2016', 60988],
+    ['Jan-2017', 138230],
+    ['Feb-2017', 671099]
 ];
 
 // ********** Financial Analysis **********
@@ -95,23 +95,23 @@ let totalNumOfMonths = finances.length;
 
 // Calculate the net total amount of Profit/Losses over the entire period
 let totalAmount = 0;
-for (let data = 0; data < finances.length; data++){
-    totalAmount += finances[data][1]; 
-} 
+for (let data = 0; data < finances.length; data++) {
+    totalAmount += finances[data][1];
+}
 
 
 // Track change in profits from month to month
 let changes = [];
 
-for (let data = 0; data < finances.length -1; data++){
-    let differenceFromMonthToMonth = 0; 
-    
+for (let data = 0; data < finances.length - 1; data++) {
+    let differenceFromMonthToMonth = 0;
+
     // initialize the array of changes with value of the first month since we assume that before that the profit was 0
-    if (data === 0){
+    if (data === 0) {
         changes.push(finances[data][1]);
-    } 
-    differenceFromMonthToMonth = finances[data+1][1] - finances[data][1];
-    changes.push(differenceFromMonthToMonth);  
+    }
+    differenceFromMonthToMonth = finances[data + 1][1] - finances[data][1];
+    changes.push(differenceFromMonthToMonth);
 }
 
 // Calculate the average of the changes in Profit/Losses over the entire period.
@@ -121,14 +121,12 @@ let averageChange = changes.reduce((acc, cum) => acc + cum, 0) / totalNumOfMonth
 // Clone then sort array of changes in order to easily extract the greatest increase in profits and the greatest decrese in losses
 let orderedChanges = [...changes].sort();
 
-// console.log(orderedChanges)
-// console.log(changes)
-
 // Get the greatest increase in profits over the entire period.
-let greatestIncreaseInProfits = orderedChanges[orderedChanges.length-1];
-let greatestIncreaseInitialIndex= changes.indexOf(greatestIncreaseInProfits);
+let greatestIncreaseInProfits = orderedChanges[orderedChanges.length - 1];
+let greatestIncreaseInitialIndex = changes.indexOf(greatestIncreaseInProfits);
 // Get the date corresponding to the greatest increase in profits over the entire period.
 let greatestIncreaseDate = finances[greatestIncreaseInitialIndex][0];
+
 
 // Get the greatest decrease in losses  over the entire period.
 let greatestDecreaseinLosses = orderedChanges[0];
@@ -139,7 +137,7 @@ let greatestDecreaseDate = finances[greatestDecreaseInitialIndex][0];
 
 
 console.log(
-`Financial Analysis
+    `Financial Analysis
 ----------------------------
 Total Months: ${totalNumOfMonths}
 Total: $${totalAmount}
